4a9645f89a6ef390232733d54f91e4ec23394b48
Ana Clara <4n4.informatica@gmail.com>
2023-11-11 23:07:58 -0300

    Finaliza o programa.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("--- Horas Extras ---\n");
+
+Console.WriteLine($"Salário por hora {15:C}, {300} horas, sendo {0,2} horas extra, salário = {Salario(15, 300, 0):C}");
+Console.WriteLine($"Salário por hora {20:C}, {350} horas, sendo {20} horas extras, salário = {Salario(20, 350, 20):C}");
+Console.WriteLine($"Salário por hora {25:C}, {400} horas, sendo {0,2} horas-extra, salário = {Salario(25, 400, 0):C}");
+Console.WriteLine($"Salário por hora {30:C}, {450} horas, sendo {20} horas extras, salário = {Salario(30, 450, 20):C}");
+
+double Salario(double salarioHora, double horasTotais, double horasExtras)
+{
+    return (salarioHora * (horasTotais - horasExtras)) + (salarioHora * horasExtras * 1.4);
+}
+
+
